a748c0967e81e05ecd490a3cfee8cbccb4648ab2
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-14 02:37:24 +0100

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a CRM created with NextJs
---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+
+        <meta name="description" content="Tuna-CRM" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-inter bg-gray-100 text-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
